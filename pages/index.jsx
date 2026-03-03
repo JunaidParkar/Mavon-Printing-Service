ae2a9c7bcb471bbd8abefb54b95a3c855d95ca4b
Junaid Parkar <100304984+JunaidParkar@users.noreply.github.com>
2026-03-03 11:33:22 +0530

    Initial commit from Create Next App

---
+++ pages/index.jsx
@@ -0,0 +1,14 @@
+"use client"
+
+import Navbar from '@/components/common/navbar'
+import React from 'react'
+
+const Page = () => {
+  return (
+    <>
+      <Navbar />
+    </>
+  )
+}
+
+export default Page
